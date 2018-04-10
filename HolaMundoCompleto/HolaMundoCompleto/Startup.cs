abf06a245454ac03bf9f092e43b8fbf7dd11a9cd
Daniellecaalle <daniellecalle@hotmail.com>
2018-04-10 17:23:25 -0500

    Subiendo archivos del proyecto

---
+++ HolaMundoCompleto/HolaMundoCompleto/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HolaMundoCompleto.Startup))]
+namespace HolaMundoCompleto
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
